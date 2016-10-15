ad8e5f8f6e5d15f863c45ab2e16fb33ce465af87
Iulii <iuliuvisovan@gmail.com>
2016-10-15 16:56:37 +0300

    Initial infrastructure

---
+++ gulpfile.js
@@ -0,0 +1,41 @@
+'use strict';
+var gulp = require('gulp');
+var sass = require('gulp-sass');
+var jade = require('gulp-jade');
+var sourcemaps = require('gulp-sourcemaps');
+var autoprefixer = require('gulp-autoprefixer');
+var browsersync = require('browser-sync').create();
+
+gulp.task('sass', () => {
+    gulp.src('./scss/*.scss')
+        .pipe(sourcemaps.init())
+        .pipe(sass({ outputStyle: 'compressed' }))
+        .pipe(sourcemaps.write())
+        .pipe(autoprefixer({
+            browsers: ['last 2 versions'],
+            cascade: false
+        }))
+        .pipe(gulp.dest('./css'))
+        .pipe(browsersync.reload({stream:true}));
+});
+
+gulp.task('jade', () => {
+    gulp.src('./*.jade')
+        .pipe(jade({
+            pretty: true
+        }))
+        .pipe(gulp.dest('./'))
+        .pipe(browsersync.reload({stream:true}));
+});
+
+gulp.task('watch', () => {
+    browsersync.init({
+        server: {
+            baseDir: '.'
+        }
+    })
+    gulp.watch('./scss/*.scss', ['sass']);
+    gulp.watch('./*.jade', ['jade']);
+})
+
+gulp.task('default', ['sass', 'jade', 'watch']);
